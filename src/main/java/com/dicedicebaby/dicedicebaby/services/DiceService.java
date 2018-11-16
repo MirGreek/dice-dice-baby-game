a251adca5012d124299f3e83923255050cf62f37
Mir <mir.greek@gmail.com>
2018-11-16 18:57:44 +0100

    diceservice got reroll and rollhand methods

--- src/main/java/com/dicedicebaby/dicedicebaby/services/DiceService.java
+++ src/main/java/com/dicedicebaby/dicedicebaby/services/DiceService.java
@@ -1,10 +1,31 @@
 package com.dicedicebaby.dicedicebaby.services;
 
 import com.dicedicebaby.dicedicebaby.models.Die;
+import com.dicedicebaby.dicedicebaby.models.Table;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface DiceService {
 
-  Die roll(Die die);
+  public Die reRoll(Die die);
+
+  public List<Die> rollHand();
+
+  long allDicesAreSame(List<Die> dice, Table table);
+
+  int fiveDicesSame();
+
+  int poker();
+
+  int fullhouse();
+
+  int straight();
+
+  int tripple();
+
+  int doublepairs();
+
+  int pair();
 }
--- src/main/java/com/dicedicebaby/dicedicebaby/services/DiceServiceImpl.java
+++ src/main/java/com/dicedicebaby/dicedicebaby/services/DiceServiceImpl.java
@@ -1,8 +1,12 @@
 package com.dicedicebaby.dicedicebaby.services;
 
 import com.dicedicebaby.dicedicebaby.models.Die;
+import com.dicedicebaby.dicedicebaby.models.Table;
 import com.dicedicebaby.dicedicebaby.repositories.DieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DiceServiceImpl implements DiceService {
 
@@ -13,8 +17,62 @@ public class DiceServiceImpl implements DiceService {
     this.dieRepository = dieRepository;
   }
 
-  public Die roll(Die die){
-    die.setValue((int) (Math.random()*6)+1);
+  public Die reRoll(Die die) {
+    die.setValue((int) (Math.random() * 6) + 1);
     return die;
   }
+
+  public List<Die> rollHand() {
+    List<Die> dice = new ArrayList<>();
+    Die die = new Die();
+    Die die2 = new Die();
+    die.setValue((int) (Math.random() * 6) + 1);
+    die2.setValue((int) (Math.random() * 6) + 1);
+    dice.add(die);
+    dice.add(die2);
+    return dice;
+  }
+
+
+  @Override
+  public long allDicesAreSame(List<Die> dice, Table table) {
+    List<Die> tableDice = table.getDice();
+    return 0;
+  }
+
+  @Override
+  public int fiveDicesSame() {
+    return 0;
+  }
+
+  @Override
+  public int poker() {
+    return 0;
+  }
+
+  @Override
+  public int fullhouse() {
+    return 0;
+  }
+
+  @Override
+  public int straight() {
+    return 0;
+  }
+
+  @Override
+  public int tripple() {
+    return 0;
+  }
+
+  @Override
+  public int doublepairs() {
+    return 0;
+  }
+
+  @Override
+  public int pair() {
+    return 0;
+  }
+
 }
